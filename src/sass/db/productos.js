724073b832e4a2b0e2434f65b1f4837e62777756
¨Dylan <dylanmegary@gmail.com>
2025-02-21 19:59:05 -0400

    se agrego y se cargo el html al main.js

---
+++ src/sass/db/productos.js
@@ -0,0 +1,46 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Rolex Oyster",
+        foto: "imgs/carlos-esteves-Ae3OHpJfngc-unsplash.webp",
+        descripcion: "Reloj Rolex Oyster", 
+        precio: 20.000
+    },
+    {
+        id: 2,
+        nombre: "Camiseta Alemania - Mundial Estados Unidos 1994",
+        foto: "imgs/alemania-1994.webp",
+        descripcion: "La camiseta titular, con un hermoso diseño que mezcla el típico color blanco con los colores de la bandera.",
+        precio: 333.30
+    },
+    {
+        id: 3,
+        nombre: "Camiseta Argentina - Mundial México 1986",
+        foto: "imgs/argentina-1986.webp",
+        descripcion: "La titular de la albiceleste, con la que el equipo de Maradona levantó el trofeo en esa competición.",
+        precio: 444.40
+    },
+    {
+        id: 4,
+        nombre: "Camiseta Argentina - Mundial Francia 1998",
+        foto: "imgs/argentina-1998.webp",
+        descripcion: "Camiseta alternativa de aquel equipazo que tenia figuras como Batitusta, Ortega y Simeone.",
+        precio: 555.50
+    },
+    {
+        id: 5,
+        nombre: "Camiseta de Boca Juniors - Copa Libertadores 2000",
+        foto: "imgs/boca-libertadores-2000.webp",
+        descripcion: "Camiseta titular de aquel equipo que con figuras como Riquelme y Palermo levantaron el trofeo continental.",
+        precio: 666.60
+    },
+    {
+        id: 6,
+        nombre: "Camiseta de Brasil - Mundial de Corea y Japón 2002",
+        foto: "imgs/brasil-2002.webp",
+        descripcion: "La titular de la verdeamarela, con la que coronaron el trofeo mundialista en aquella ocasión.",
+        precio: 666.60
+    },
+]
+
+export default productos
