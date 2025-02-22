f30a7e73e1163a88b8b1d1313e3d8016dfdf757c
¨Dylan <dylanmegary@gmail.com>
2025-02-21 20:15:44 -0400

    se arreglo algo de main.js

--- src/main.js
+++ src/main.js
@@ -7,19 +7,20 @@ const start = () => {
     let html = ''
     productos.forEach(prod => {
         console.log(prod)
-        html += `<div class="card">
-            <article class="card__article">
-                <div class="card__image-container">
+        html += `<div class="card"> 
+                <article class="card__article">
+                  <div class="card__img-container">
                     <img class="card__image" src="${prod.foto}" alt="${prod.nombre}">
+                      </div>
+                       <div class="card__content"></div>
+                        <h2 class="card__heading">${prod.nombre}</h2>
+                         <div class="card__descrption">${prod.descripcion}<p>
+                            </p> 
+                         </div>
+                      </div>
+                   </article>
                 </div>
-                <div class="card__content">
-                    <h2 class="card__heading">${prod.nombre}</h2>
-                    <div class="card__description">
-                        <p>${prod.descripcion}</p>
-                    </div>
-                </div>
-            </article>
-        </div>`
+`
     })
     contenedorProductos.innerHTML = html
 }
