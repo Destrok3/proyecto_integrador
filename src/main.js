1a4c6f7beda0124e9f8d7df3d7f4c01dd2e69958
¨Dylan <dylanmegary@gmail.com>
2025-02-21 20:13:37 -0400

    se arreglo algo de main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
 import './sass/main.scss'
 
 const contenedorProductos = document.getElementById('container-productos')
+
+const start = () => {
+    console.warn('Se cargó todo el HTML')
+    let html = ''
+    productos.forEach(prod => {
+        console.log(prod)
+        html += `<div class="card">
+            <article class="card__article">
+                <div class="card__image-container">
+                    <img class="card__image" src="${prod.foto}" alt="${prod.nombre}">
+                </div>
+                <div class="card__content">
+                    <h2 class="card__heading">${prod.nombre}</h2>
+                    <div class="card__description">
+                        <p>${prod.descripcion}</p>
+                    </div>
+                </div>
+            </article>
+        </div>`
+    })
+    contenedorProductos.innerHTML = html
+}
