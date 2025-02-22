1243d698d1893f33d3fd7eddd5798e8ea381a4e9
¨Dylan <dylanmegary@gmail.com>
2025-02-21 20:09:35 -0400

    lista de productos terminada

--- src/sass/db/productos.js
+++ src/sass/db/productos.js
@@ -4,42 +4,58 @@ const productos = [
         nombre: "Rolex Oyster",
         foto: "imgs/carlos-esteves-Ae3OHpJfngc-unsplash.webp",
         descripcion: "Reloj Rolex Oyster", 
-        precio: 20.000
+        precio: 20000.00
     },
     {
         id: 2,
-        nombre: "Camiseta Alemania - Mundial Estados Unidos 1994",
-        foto: "imgs/alemania-1994.webp",
-        descripcion: "La camiseta titular, con un hermoso diseño que mezcla el típico color blanco con los colores de la bandera.",
-        precio: 333.30
+        nombre: "Seiko Automatic Divers",
+        foto: "imgs/darryl-low-wPUwlOQ6gcU-unsplash.webp",
+        descripcion: "Hermoso Reloj Seiko",
+        precio: 600.00
     },
     {
         id: 3,
-        nombre: "Camiseta Argentina - Mundial México 1986",
-        foto: "imgs/argentina-1986.webp",
-        descripcion: "La titular de la albiceleste, con la que el equipo de Maradona levantó el trofeo en esa competición.",
-        precio: 444.40
+        nombre: "Patek Philliep Nautilus"
+        foto: "imgs/diniy-salleh-gaBYtKLzf-I-unsplash.webp",
+        descripcion: "Reloj Patek Philliep Nautilus",
+        precio: 15000.00
     },
     {
         id: 4,
-        nombre: "Camiseta Argentina - Mundial Francia 1998",
-        foto: "imgs/argentina-1998.webp",
-        descripcion: "Camiseta alternativa de aquel equipazo que tenia figuras como Batitusta, Ortega y Simeone.",
-        precio: 555.50
+        nombre: "Seiko Automatic Divers 200m",
+        foto: "imgs/jonathan-mabey-Gi4RVQcre7w-unsplash.webp",
+        descripcion: "Reloj Seiko Automatic pero azul y rojo modelo para buceadores",
+        precio: 600.00
     },
     {
         id: 5,
-        nombre: "Camiseta de Boca Juniors - Copa Libertadores 2000",
-        foto: "imgs/boca-libertadores-2000.webp",
-        descripcion: "Camiseta titular de aquel equipo que con figuras como Riquelme y Palermo levantaron el trofeo continental.",
-        precio: 666.60
+        nombre: "Rolex Datejust",
+        foto: "imgs/laurenz-heymann-al6s6JpnZis-unsplash.webp",
+        descripcion: "Hermoso Rolex Datejust",
+        precio: 25000.00
     },
     {
         id: 6,
-        nombre: "Camiseta de Brasil - Mundial de Corea y Japón 2002",
-        foto: "imgs/brasil-2002.webp",
-        descripcion: "La titular de la verdeamarela, con la que coronaron el trofeo mundialista en aquella ocasión.",
-        precio: 666.60
+        nombre: "Rolex Datejust Endimantado",
+        foto: "imgs/sabrianna--0ZHq2f8LAo-unsplash.webp",
+        descripcion: "Rolex Datejust ahora con diamantes hermosos",
+        precio: 35000.00
+    },
+
+    {
+        id: 7,
+        nombre: "Tissot V8",
+        foto: "imgs/ar-7P1suWK57fY-unsplash.webp",
+        descripcion: "Tissot V8 un reloj sencillo pero hermoso",
+        precio: 300.00
+    },
+
+    {
+        id: 8,
+        nombre: "Breitling Chronomat Automatic GMT 40",
+        foto: "imgs/frank-park-nXruwpNrOzQ-unsplash.webp",
+        descripcion: "Breitling un reloj hermoso de uan marca muy reconocida",
+        precio: 8350.00
     },
 ]
 
